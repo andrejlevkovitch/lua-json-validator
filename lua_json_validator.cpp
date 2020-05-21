cf7d1c8e9aea8c15d0e4205d80e989dbbbd652bb
andrejlevkovitch <andrejlevkovitch@gmail.com>
2020-05-21 15:45:14 +0300

    add factory method for validator

--- lua_json_validator.cpp
+++ lua_json_validator.cpp
@@ -6,7 +6,9 @@
 #include <sstream>
 #include <string>
 
-using json = nlohmann::json;
+#define VALIDATOR_INSTANCE_METATABLE "json_validator_instance"
+
+using json           = nlohmann::json;
 using json_validator = nlohmann::json_schema::json_validator;
 
 const json draft7 = json::parse(R"(
@@ -182,9 +184,9 @@ const json draft7 = json::parse(R"(
 
 class ValidatorErrorHandler
     : public nlohmann::json_schema::basic_error_handler {
-  void error(const nlohmann::json::json_pointer &ptr,
+  void error(const nlohmann::json::json_pointer &   ptr,
              [[maybe_unused]] const nlohmann::json &instance,
-             const std::string &message) override {
+             const std::string &                    message) override {
     ss << "Error: '" << ptr << "' : " << message << std::endl;
 
     nlohmann::json_schema::basic_error_handler::error(ptr, instance, message);
@@ -202,7 +204,7 @@ extern "C" {
  */
 static int lua_json_schema_validate(lua_State *state) {
   std::string schemaStr = luaL_checkstring(state, 1);
-  std::string jsonStr = luaL_checkstring(state, 2);
+  std::string jsonStr   = luaL_checkstring(state, 2);
 
   json schema = json::parse(schemaStr, nullptr, false);
   if (schema.is_discarded()) {
@@ -228,7 +230,7 @@ static int lua_json_schema_validate(lua_State *state) {
   }
 
   ValidatorErrorHandler error;
-  json patch = validator.validate(forValidation, error);
+  json                  patch = validator.validate(forValidation, error);
   if (error) {
     lua_pushnil(state);
     lua_pushstring(state, error.ss.str().c_str());
@@ -239,7 +241,81 @@ static int lua_json_schema_validate(lua_State *state) {
                        // changes and without copy
     lua_pushvalue(state, 2);
   } else { // in this case aply some patch and return patched json string
-    json outputJson = forValidation.patch(patch);
+    json        outputJson = forValidation.patch(patch);
+    std::string outJsonStr = outputJson.dump();
+    lua_pushlstring(state, outJsonStr.c_str(), outJsonStr.size());
+  }
+
+  return 1;
+}
+
+/**\brief create new instance of validator with some schema
+ */
+static int lua_json_validator_new(lua_State *state) {
+  std::string schemaStr = luaL_checkstring(state, 1);
+
+  json schema = json::parse(schemaStr, nullptr, false);
+  if (schema.is_discarded()) {
+    lua_pushnil(state);
+    lua_pushstring(state, "invalid json");
+    return 2;
+  }
+
+  // end push validator instance as table
+  void *          addr      = lua_newuserdata(state, sizeof(json_validator));
+  json_validator *validator = new (addr) json_validator{};
+
+  try {
+    validator->set_root_schema(schema);
+  } catch (std::exception &e) {
+    validator->~json_validator();
+
+    lua_pop(state, 1);
+
+    lua_pushnil(state);
+    lua_pushstring(state, "invalid schema");
+    return 2;
+  }
+
+  luaL_getmetatable(state, VALIDATOR_INSTANCE_METATABLE);
+  lua_setmetatable(state, -2);
+
+  return 1;
+}
+
+static int lua_json_validator_destroy(lua_State *state) {
+  void *addr = luaL_checkudata(state, 1, VALIDATOR_INSTANCE_METATABLE);
+  json_validator *validator = reinterpret_cast<json_validator *>(addr);
+  validator->~json_validator();
+  return 0;
+}
+
+/**\brief validate json
+ * \param 1 self
+ * \param 2 json as strign
+ * \return input json as string in case of success, othewise nil and error
+ * string
+ */
+static int lua_json_validator_validate(lua_State *state) {
+  void *      addr = luaL_checkudata(state, 1, VALIDATOR_INSTANCE_METATABLE);
+  std::string forCheckStr = luaL_checkstring(state, 2);
+
+  json_validator *validator    = reinterpret_cast<json_validator *>(addr);
+  json            forCheckJson = json::parse(forCheckStr, nullptr, false);
+
+  ValidatorErrorHandler error;
+  json                  patch = validator->validate(forCheckJson, error);
+  if (error) {
+    lua_pushnil(state);
+    lua_pushstring(state, error.ss.str().c_str());
+    return 2;
+  }
+
+  if (patch.empty()) { // in this case just push original string without any
+                       // changes and without copy
+    lua_pushvalue(state, 2);
+  } else { // in this case aply some patch and return patched json string
+    json        outputJson = forCheckJson.patch(patch);
     std::string outJsonStr = outputJson.dump();
     lua_pushlstring(state, outJsonStr.c_str(), outJsonStr.size());
   }
@@ -275,11 +351,31 @@ static int lua_json_schema_check(lua_State *state) {
   return 1;
 }
 
-static luaL_Reg json_validator_table[]{{"check_schema", lua_json_schema_check},
-                                       {"validate", lua_json_schema_validate},
-                                       {nullptr, nullptr}};
+static luaL_Reg json_validator_table[]{
+    {"check_schema", lua_json_schema_check},
+    {"validate", lua_json_schema_validate},
+    {"new", lua_json_validator_new},
+    {nullptr, nullptr},
+};
+
+static luaL_Reg json_validator_metatable[]{
+    {"__gc", lua_json_validator_destroy},
+    {"validate", lua_json_validator_validate},
+    {nullptr, nullptr},
+};
 
 int luaopen_json_validator(lua_State *state) {
+  // at first register metatable for validator
+  luaL_newmetatable(state, VALIDATOR_INSTANCE_METATABLE);
+  lua_pushvalue(state, -1);
+  lua_setfield(state, -2, "__index");
+  for (int i = 0; json_validator_metatable[i].name != nullptr; ++i) {
+    lua_pushcfunction(state, json_validator_metatable[i].func);
+    lua_setfield(state, -2, json_validator_metatable[i].name);
+  }
+  lua_pop(state, 1);
+
+  // and register module
   luaL_register(state, "json_validator", json_validator_table);
   return 1;
 }
